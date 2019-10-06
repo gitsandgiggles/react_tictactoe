85dfb785a350086fbed64719b8c29d3557e13f81
gitsandgiggles <github@thebubble.co.uk>
2019-10-06 22:43:19 +0100

    completed main tutorial

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -57,6 +57,7 @@ function Square (props) {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
@@ -65,8 +66,19 @@ function Square (props) {
 
 
       const history = this.state.history;
-      const current = history[history.length -1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map ( (step, move) => {
+          const desc = move ? 
+            'Go to move #' + move : 
+            'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick= {() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+      });
 
       let status;
       if (winner) {
@@ -86,7 +98,7 @@ function Square (props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
@@ -95,7 +107,7 @@ function Square (props) {
 
     handleClick(i) {
 
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length -1];
         const squares = current.squares.slice();
        
@@ -108,10 +120,18 @@ function Square (props) {
             history : history.concat( [ { 
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2 ) === 0,
+        })
+    }
 
   }
   
